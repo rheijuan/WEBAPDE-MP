4c638a75a8514d575915143b4f970dc4d4c05e6e
Your Name <you@example.com>
2018-07-11 18:45:14 +0800

    connected pages

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "../WEBAPDE-MP1/html/start.html"));
 });
 
+app.get("/home.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../WEBAPDE-MP1/html/home.html"))
+});
+
+app.get("/register.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../WEBAPDE-MP1/html/register.html"))
+});
+
+app.get("/start.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../WEBAPDE-MP1/html/start.html"))
+});
+
 app.listen(3000, () => {
     console.log("Listening in port 3000");
 });
