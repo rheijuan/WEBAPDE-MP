50ef746842a68c872166450e4564a92c59289f5b
Jaymee Villacruz <36391939+jaymee143@users.noreply.github.com>
2018-08-25 19:40:27 +0800

    editPage.hbs has content but not finished/final

--- server.js
+++ server.js
@@ -115,6 +115,11 @@ app.post("/okay", (req,res) => {
     res.render("home.hbs")
 })
 
+app.get("/editPage", (req, res)=>{
+    console.log("POST /editPage")
+    
+    res.render("editPage.hbs")
+})
 app.post("/add", urlencoder, (req, res) => {
     console.log("POST /ADD")
 
