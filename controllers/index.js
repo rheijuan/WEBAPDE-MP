c2f86a1c8e673c7e6eb3b09f08e51429df71f52e
Rhei Justin Juan <rhei_juan@dlsu.com>
2018-08-28 00:52:42 +0800

    still trying to fix cookies

--- controllers/index.js
+++ controllers/index.js
@@ -10,39 +10,42 @@ const cookieparser = require("cookie-parser")
 router.use("/reservation", require("./reservation"))
 router.use("/user", require("./user"))
 
-router.use("/", (req,res, next) => {
+// router.use("/", (req,res, next) => {
 
-    if(req.cookies.username) 
-        res.locals.username = req.cookies.username
+//     if(req.cookies.username) 
+//         res.locals.username = req.cookies.username
 
-    next()
-})
+//     next()
+// })
 
 router.get("/", function(req, res) {
     console.log("GET /")
+    console.log(req.cookies.username)
+    console.log(req.user)
 
-    if(res.locals.username) {
-        res.render("home", {
-            username: res.locals.username
-        })
-    } else 
-        res.render("index")
+    res.render("index")
 })
 
 router.get("/register", function(req, res) {
     console.log("GET /register")
+    console.log(req.cookies.username)
+
     res.render("register.hbs")
 })
 
 router.get("/home", function(req, res) {
     console.log("GET /home")
-    
+    console.log(req.cookies.username)
+    console.log(req.user)
+
     res.render("home", {
-        username: res.locals.username
+        username: req.cookies.username
     })
 })
 
 router.post("/logout", function(req, res) {
+    req.session.destroy
+
     res.render("index")
 })
 
